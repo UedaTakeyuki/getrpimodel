5e75efe792df627ea89a64956c5f3ad5f0ad0534
Ueda Takeyuki <ueda@latelierdueda.com>
2017-02-21 20:57:41 +0900

    fix:

--- getrpimodel/__init__.py
+++ getrpimodel/__init__.py
@@ -7,20 +7,21 @@ import re
 import sys
 
 
-usage = 'Usage: python {} [--s]'.format(__file__)
+usage = 'Usage: python -m getrpimodel [--s]'
 
 # model definition table from revision info.
 # refer http://elinux.org/RPi_HardwareHistory
 model_a          = ["0007","0008","0009",]
-model_b          = ["0002","0004","0005","0006",]
+model_b          = ["0002","0004","0005","0006","000d","000e","000e",]
 model_b_beta     = ["Beta",]
 model_b_ECN0001  = ["0003",]
-model_cm         = ["0011","0011",]
-model_a_plus     = ["0012","0015",]
+model_cm         = ["0011","0014",]
+model_cm3        = ["a020a0",]
+model_a_plus     = ["0012","0015","900021",]
 model_b_plus     = ["0010","0013",]
 model_2b         = ["a01040","a01041","a21041",]
 model_2b_2837    = ["a22042",]
-model_3b         = ["a02082", "a22082",]
+model_3b         = ["a02082", "a22082","a32082",]
 model_zero       = ["900092","900093","920093",]
 
 def revision():
@@ -45,6 +46,8 @@ def model_strict():
     return "B (ECN0001)"
   elif rev in model_cm:
     return "Compute Module"
+  elif rev in model_cm3:
+    return "Compute Module 3(and CM3 Lite)"
   elif rev in model_a_plus:
     return "A+"
   elif rev in model_b_plus:
@@ -58,7 +61,8 @@ def model_strict():
   elif rev in model_zero:
     return "Zero"
   else:
-    return None
+    return rev
+#    return None
 
 def model():
 #  rev = getpirevision.revision()
@@ -67,7 +71,7 @@ def model():
     return "A"
   elif rev in model_b + model_b_beta + model_b_ECN0001:
     return "B"
-  elif rev in model_cm:
+  elif rev in model_cm + model_cm3:
     return "Compute Module"
   elif rev in model_a_plus:
     return "A+"
@@ -80,7 +84,8 @@ def model():
   elif rev in model_zero:
     return "Zero"
   else:
-    return None
+    return rev
+#    return None
 
 if __name__ == '__main__':
   if len(sys.argv) == 1:
