37757c91fc4197e32433b7918acd556976802cdc
Dr. Takeyuki Ueda <ueda@latelierdueda.com>
2018-09-20 10:22:28 +0900

    version 0.1.14

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
+
+with open("README.md") as f:
+  long_description = f.read()
+
 setup(
   name = 'getrpimodel',
   packages = ['getrpimodel'], # this must be the same as the name above
-  version = '0.1.13',
+  version = '0.1.14',
   description = 'Get Raspberry Pi model Name(eg: A, B, B+...)',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   author = 'Takeyuki UEDA',
   author_email = 'gde00107@nifty.com',
   license='MIT',
